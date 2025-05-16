59e83d63a55879fba46902f6ee92a5a5a2a32560
Edwin Fernando <ef322@ic.ac.uk>
2025-05-16 11:32:09 +0100

    RISC-V: fix incorrect nonce counters and keypair for each address
    # Conflicts:
    #	src/riscv/revm/bench/src/generate.rs

--- src/riscv/revm/bench/src/generate.rs
+++ src/riscv/revm/bench/src/generate.rs
@@ -7,10 +7,10 @@ use std::path::Path;
 use std::vec;
 
 use alloy_sol_types::{SolCall, sol};
-use jstz_crypto::keypair_from_passphrase;
+use jstz_crypto::{keypair_from_passphrase, public_key::PublicKey, secret_key::SecretKey};
 use revm::{
     context::TxEnv,
-    primitives::{Address, TxKind, U256, address, hex},
+    primitives::{Address, Bytes, TxKind, U256, address, hex},
 };
 use tezos_data_encoding::enc::BinWriter;
 use tezos_smart_rollup::inbox::ExternalMessageFrame;
@@ -24,9 +24,8 @@ use utils::crypto::SignedOperation;
 const GLD_CONTRACT_BYTECODE: &str = include_str!("../../contract.bin");
 // This is fragile since it is hardcoded for the GLDToken contract of originator with address 0x1
 const CONTRACT_ADDRESS: Address = address!("Bd770416a3345F91E4B34576cb804a576fa48EB1");
-const ALICE: Address = address!("647384899878C28EC3F61d100daF4d40471f1852"); // random
-const BOB: Address = address!("983267588688C28EC3F61d100daF4d40471f1853"); // random
-const MINTER: Address = address!("0000000000000000000000000000000000000001");
+// Big enough that it doesn't clash with the 0..num accounts
+const MINTER: Address = address!("9999999999999999999999999999999999999999");
 const EXTERNAL_FRAME_SIZE: usize = 21;
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
@@ -53,146 +52,159 @@ pub fn handle_generate_script(
 }
 
 fn generate_inbox(rollup_addr: &str, transfers: usize) -> Result<InboxFile> {
-    let messages = create_operations(transfers)?
-        .into_iter()
-        .enumerate()
-        .map(|(i, tx)| generate_message(rollup_addr, tx, i))
-        .collect::<Result<Vec<_>>>()?;
+    let rollup_addr = SmartRollupAddress::from_b58check(rollup_addr)?;
+    let messages = create_operations(&rollup_addr, transfers)?;
 
     // Output inbox file
     Ok(InboxFile(vec![messages]))
 }
 
-/// `TxEnv` is the type a transaction on ethereum (revm). We serialize these transactions using the
-/// external message frame protocol
-/// We also sign the message with some generated public-private key pair. As its just a
-/// benchmark we don't care that each address should correspond to a pair
-fn generate_message(rollup_addr: &str, tx: TxEnv, i: usize) -> Result<Message> {
-    let rollup_addr = SmartRollupAddress::from_b58check(rollup_addr)?;
-    // Create signed operation
-    let (sk, pk) = keypair_from_passphrase(&i.to_string())?;
-    let op = Operation(tx);
-    let sig = sk.sign(op.hash()?)?;
-    let signed_op = SignedOperation::new(pk.clone(), sig, op);
-    let bytes = bincode::serde::encode_to_vec(&signed_op, bincode::config::standard())?;
-    let mut external = Vec::with_capacity(bytes.len() + EXTERNAL_FRAME_SIZE);
-    let frame = ExternalMessageFrame::Targetted {
-        contents: bytes,
-        address: rollup_addr,
-    };
-
-    frame.bin_write(&mut external)?;
-
-    Ok(Message::External { external })
+struct Account {
+    nonce: u64,
+    sk: SecretKey,
+    pk: PublicKey,
+    address: Address,
 }
 
-/// Hardcode some operations to be sent through the inbox file
+impl Account {
+    /// `TxEnv` is the type a transaction on ethereum (revm). We serialize these transactions using the
+    /// external message frame protocol
+    fn operation_to_message(
+        &mut self,
+        rollup_addr: &SmartRollupAddress,
+        kind: TxKind,
+        abi_call: Bytes,
+    ) -> Result<Message> {
+        let tx = TxEnv {
+            kind,
+            data: abi_call,
+            caller: self.address,
+            nonce: self.nonce,
+            ..TxEnv::default()
+        };
+        self.nonce += 1;
+        // Create signed operation
+        let op = Operation(tx);
+        let sig = self.sk.sign(op.hash()?)?;
+        let signed_op = SignedOperation::new(self.pk.clone(), sig, op);
+        let bytes = bincode::serde::encode_to_vec(&signed_op, bincode::config::standard())?;
+        let mut external = Vec::with_capacity(bytes.len() + EXTERNAL_FRAME_SIZE);
+        let frame = ExternalMessageFrame::Targetted {
+            contents: bytes,
+            address: rollup_addr.clone(),
+        };
+
+        frame.bin_write(&mut external)?;
+
+        Ok(Message::External { external })
+    }
+}
+
 /// 1. Deploy the GLDToken ERC20 contract
-/// 2. Mint 100 coins to Alice from the owner address (0x1)
-/// 3. Alice sends 100 coins to Bob
-/// 4. Alice's balance gets queried
-fn create_operations(transfers: usize) -> Result<Vec<TxEnv>> {
-    let mut operations = Vec::new();
+/// 2. Mint fixed amount of coins to each address
+/// 3. Generate trasnfers between the accounts
+/// 4. Query balance of all accounts
+fn create_operations(rollup_addr: &SmartRollupAddress, transfers: usize) -> Result<Vec<Message>> {
+    // setup
+    let mut messages = Vec::new();
+
+    let (sk, pk) = keypair_from_passphrase("foobar")?;
+    let mut minter = Account {
+        nonce: 0,
+        sk,
+        pk,
+        address: MINTER,
+    };
+
+    let len = accounts_for_transfers(transfers);
+    let mut accounts: Vec<Account> = (0..len)
+        .map(|i| {
+            let (sk, pk) = keypair_from_passphrase(&i.to_string())?;
+            Ok(Account {
+                nonce: 0,
+                sk,
+                pk,
+                address: Address::left_padding_from(&usize::to_be_bytes(i)),
+            })
+        })
+        .collect::<Result<_>>()?;
 
     // deploy the contract
     let bytecode: Vec<u8> = hex::decode(GLD_CONTRACT_BYTECODE)?;
+    messages.push(minter.operation_to_message(rollup_addr, TxKind::Create, bytecode.into())?);
 
-    operations.push(TxEnv {
-        kind: TxKind::Create,
-        data: bytecode.into(),
-        ..TxEnv::default()
-    });
+    // mint coins for everyone
 
-    let len = accounts_for_transfers(transfers);
-    let addrs: Vec<Address> = (0..len)
-        .into_iter()
-        .map(|i| Address::left_padding_from(&usize::to_be_bytes(i)))
-        .collect();
-
-    // Mint coins for everyone
-
-    let mut nonce = 0; // nonce for minting
     // Generate abi for the function we want to call from the contract
     // Solidity source code from https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v5.3.0/contracts/token/ERC20/IERC20.sol
     // and my extension of it `GLDToken.sol`
     sol! {
-        function mint(address to, uint256 amount) public onlyOwner;
+        function mint(address to, uint256 amount) public;
     }
 
     let amount = len + 1;
-    for i in 0..len {
+    for acc in &accounts {
         let mint_call = mintCall {
-            to: ALICE,
+            to: acc.address,
             amount: U256::from(amount),
         }
         .abi_encode();
-        let op = TxEnv {
-            kind: TxKind::Call(CONTRACT_ADDRESS),
-            data: mint_call.into(),
-            caller: MINTER,
-            nonce: nonce,
-            ..TxEnv::default()
-        };
-        operations.push(op);
-        nonce += 1;
+        let msg = minter.operation_to_message(
+            rollup_addr,
+            TxKind::Call(CONTRACT_ADDRESS),
+            mint_call.into(),
+        )?;
+        messages.push(msg);
     }
 
     // Generate transfers
-
-    let mut nonces = vec![0; len]; // for transfers
 
     sol! {
         function transfer(address to, uint256 value) external returns (bool);
     }
 
-    let alice_to_bob = transferCall {
-        to: BOB,
-        value: U256::from(40),
-    }
-    .abi_encode();
-
-    let expected_len = operations.len() + transfers;
+    let expected_len = messages.len() + transfers;
 
     'outer: for token_id in 0..len {
         for (from, amount) in (token_id..(token_id + len)).zip(1..len) {
-            if expected_len == operations.len() {
+            if expected_len == messages.len() {
                 break 'outer;
             }
 
-            let to = addrs[(from + 1) % len];
-            let from_addr = addrs[from % len];
             let call_data = transferCall {
-                to,
+                to: accounts[(from + 1) % len].address,
                 value: U256::from(len - amount),
             }
             .abi_encode();
-            let op = TxEnv {
-                kind: TxKind::Call(CONTRACT_ADDRESS),
-                data: call_data.into(),
-                caller: from_addr,
-                nonce: nonces[from % len],
-                ..TxEnv::default()
-            };
-            operations.push(op);
-            nonces[from % len] += 1;
+            let msg = accounts[from % len].operation_to_message(
+                rollup_addr,
+                TxKind::Call(CONTRACT_ADDRESS),
+                call_data.into(),
+            )?;
+            messages.push(msg);
         }
     }
 
-    // Query ALICE's balance
-    //sol! {
-    //    function balanceOf(address account) external view returns (uint256);
-    //}
+    // Query everyone's balance
 
-    //let query = balanceOfCall { account: ALICE }.abi_encode();
+    sol! {
+        function balanceOf(address account) external view returns (uint256);
+    }
 
-    //operations.push(TxEnv {
-    //    kind: TxKind::Call(CONTRACT_ADDRESS),
-    //    data: query.into(),
-    //    nonce: 1,
-    //    ..TxEnv::default()
-    //});
+    for acc in &accounts {
+        let balance_call = balanceOfCall {
+            account: acc.address,
+        }
+        .abi_encode();
+        let msg = minter.operation_to_message(
+            rollup_addr,
+            TxKind::Call(CONTRACT_ADDRESS),
+            balance_call.into(),
+        )?;
+        messages.push(msg);
+    }
 
-    Ok(operations)
+    Ok(messages)
 }
 
 /// The generation strategy supports up to `num_accounts ^ 2` transfers,
