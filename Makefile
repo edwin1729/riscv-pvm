7970c3afcd2a3638d704e96606663794f01fddc7
Edwin Fernando <edwin.fernando@nomadic-labs.com>
2025-07-24 14:26:07 +0200

    Move revm kernel

--- Makefile
+++ Makefile
@@ -4,42 +4,49 @@
 
 ### Generic top-level targets
 
-all: riscv/all jstz/all dummy/all block-cache-tester/all etherlink/all
+all: riscv/all jstz/all dummy/all block-cache-tester/all etherlink/all revm/all
 
-build-deps: riscv/build-deps jstz/build-deps etherlink/build-deps
+build-deps: riscv/build-deps jstz/build-deps etherlink/build-deps revm/build-deps
 
 build-deps-slim: riscv/build-deps-slim
 
-check: riscv/check jstz/check dummy/check block-cache-tester/all etherlink/check
+check: riscv/check jstz/check dummy/check block-cache-tester/all etherlink/check revm/check
 
 audit: riscv/audit
 
-build: riscv/build jstz/build dummy/build block-cache-tester/build etherlink/build
+build: riscv/build jstz/build dummy/build block-cache-tester/build etherlink/build revm/build revm/inbox-bench
 
-test: riscv/test jstz/test etherlink/test 
+run-revm: revm/build
+	@cargo run --manifest-path=kernels/revm/Cargo.toml --bin inbox-bench --release -- generate --address "sr163Lv22CdE8QagCwf48PWDTquk6isQwv57" --transfers 16
+	@cargo run --manifest-path src/riscv/Cargo.toml --release -- run -m supervisor --address "sr163Lv22CdE8QagCwf48PWDTquk6isQwv57" --input kernels/revm/target/riscv64gc-unknown-linux-musl/release/revm-kernel --inbox-file inbox.json
+
+test: riscv/test jstz/test etherlink/test revm/test
 
 test-long: riscv/test-long
 
 test-miri: riscv/test-miri
 
-clean: riscv/clean jstz/clean dummy/clean block-cache-tester/clean etherlink/clean
+clean: riscv/clean jstz/clean dummy/clean block-cache-tester/clean etherlink/clean revm/clean
 
 ### Target proxies
 
-riscv/%: 
+riscv/%:
 	@make -C src/riscv ${@:riscv/%=%}
 
-jstz/%: 
+jstz/%:
 	@make -C kernels/jstz ${@:jstz/%=%}
 
-dummy/%: 
+dummy/%:
 	@make -C kernels/dummy ${@:dummy/%=%}
 
-block-cache-tester/%: 
+block-cache-tester/%:
 	@make -C kernels/block-cache-tester ${@:block-cache-tester/%=%}
 
-etherlink/%: 
+etherlink/%:
 	@make -C kernels/etherlink ${@:etherlink/%=%}
 
+revm/%:
+	@make -C kernels/revm ${@:revm/%=%}
+
 # Mark all non-pattern targets as phony to make sure they're always executed
-.PHONY: all build-deps build-deps-slim check audit build test test-long test-miri clean 
+.PHONY: all build-deps build-deps-slim check audit build test test-long test-miri clean
---
+++ kernels/revm/utils/src/crypto.rs
@@ -0,0 +1,107 @@
+// SPDX-FileCopyrightText: 2025 Nomadic Labs <contact@nomadic-labs.com>
+//
+// SPDX-License-Identifier: MIT
+
+pub use libsecp256k1::{curve::Scalar, PublicKey, SecretKey};
+use libsecp256k1::{sign, Error, Message, Signature};
+
+#[cfg(not(target_arch = "riscv64"))]
+use libsecp256k1::verify;
+use revm::context::TxEnv;
+use serde::{Deserialize, Serialize};
+use sha3::{Digest, Keccak256};
+//use crate::sbi_crypto::secp256k1_verify;
+
+#[cfg(target_arch = "riscv64")]
+mod sbi;
+
+/// All the data for an evm operation is encoded in the `TxEnv` struct.
+/// The wrapper is to add a hash method.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Operation(pub TxEnv);
+
+impl From<&Operation> for Vec<u8> {
+    fn from(op: &Operation) -> Self {
+        bincode::serde::encode_to_vec(&op.0, bincode::config::standard()).unwrap()
+    }
+}
+
+/// The Operation along with it's `signature` and `address` for verification
+#[derive(Serialize, Deserialize)]
+pub struct SignedOperation {
+    // public key
+    pub pk: PublicKey,
+    #[serde(with = "serde_sig")]
+    signature: Signature,
+    inner: Operation,
+}
+
+mod serde_sig {
+    use super::*;
+    use serde::de::Error as DeError;
+    use serde::{Deserialize, Deserializer, Serializer};
+    pub fn serialize<S>(sig: &Signature, s: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let bytes = sig.serialize();
+        s.serialize_bytes(bytes.as_slice())
+    }
+    pub fn deserialize<'de, D>(d: D) -> Result<Signature, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let bytes: Vec<u8> = Deserialize::deserialize(d)?;
+        let exact_bytes: &[u8; 64] = bytes.as_slice().try_into().map_err(DeError::custom)?;
+        Signature::parse_standard(exact_bytes).map_err(DeError::custom)
+    }
+}
+
+impl SignedOperation {
+    /// Create a `SignedOperation`
+    pub fn sign(pk: PublicKey, sk: SecretKey, inner: Operation) -> Self {
+        let signature = sign(&Self::message_from_op(&inner), &sk).0;
+        Self {
+            pk,
+            signature,
+            inner,
+        }
+    }
+    fn message_from_op(op: &Operation) -> Message {
+        let bytes: Vec<u8> = (op).into();
+        let hash: [u8; 32] = Keccak256::digest(bytes).into();
+        Message::parse(&hash)
+    }
+
+    /// return the payload if the signature is valid
+    /// Only for native compilation. Otherwise the host function found in `crypto/sbi.rs` is used
+    #[cfg(not(target_arch = "riscv64"))]
+    pub fn verify(self) -> Option<Operation> {
+        verify(
+            &Self::message_from_op(&self.inner),
+            &self.signature,
+            &self.pk,
+        )
+        .then_some(self.inner)
+    }
+
+    /// For benchmarks not using verification of signatures of each transaction
+    pub fn no_verify(self) -> Option<Operation> {
+        Some(self.inner)
+    }
+}
+
+pub fn address_from_pk(pk: &PublicKey) -> [u8; 20] {
+    // Drop the initial byte which is a tag and hash
+    let hash: [u8; 32] = Keccak256::digest(&pk.serialize()[1..]).into();
+
+    let mut addr = [0u8; 20];
+    addr.copy_from_slice(&hash[12..]);
+    addr
+}
+
+pub fn keypair_from_int(n: u32) -> Result<(SecretKey, PublicKey), Error> {
+    let sk = SecretKey::try_from(Scalar::from_int(n))?;
+    let pk = PublicKey::from_secret_key(&sk);
+    Ok((sk, pk))
+}
